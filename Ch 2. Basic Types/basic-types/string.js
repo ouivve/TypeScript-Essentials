58eaa3d9476e3667b16213221c52b01e10fd4fe2
ouivve <ouivve@gmail.com>
2022-03-23 16:37:55 +0900

    add: 2.5 string

---
+++ Ch 2. Basic Types/basic-types/string.js
@@ -0,0 +1,14 @@
+"use strict";
+let fullName = `YooJung Jung`;
+let age = 26;
+let sentence = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next year.`;
+console.log(sentence);
+// template string을 사용하지 않을 경우
+// let sentence: string =
+//   "Hello, my name is " +
+//   fullName +
+//   ".\n" +
+//   "I'll be " +
+//   (age + 1) +
+//   " years old next year.";
---
+++ Ch 2. Basic Types/basic-types/string.ts
@@ -0,0 +1,17 @@
+let fullName: string = `YooJung Jung`;
+let age: number = 26;
+
+let sentence: string = `Hello, my name is ${fullName}.
+I'll be ${age + 1} years old next year.`;
+
+console.log(sentence);
+
+// template string을 사용하지 않을 경우
+
+// let sentence: string =
+//   "Hello, my name is " +
+//   fullName +
+//   ".\n" +
+//   "I'll be " +
+//   (age + 1) +
+//   " years old next year.";
