ab22ac42637f5feffdd7fe3efe120f2c7b0888dd
ouivve <ouivve@gmail.com>
2022-03-24 13:42:15 +0900

    add: 2.8 object

---
+++ Ch 2. Basic Types/basic-types/object.ts
@@ -0,0 +1,11 @@
+// create by object lieteral
+const person1 = { name: "Ouivve", age: 26 };
+// const person1: {
+//    name: string;
+//    age: number;
+// }
+
+// create by Object.create
+const person2 = Object.create({ name: "Ouivve", age: 26 });
+
+// (method) ObjectConstructor.create(o: object | null): any (+1 overload)
