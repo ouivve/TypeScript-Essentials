3b25494b111096fa2739c5bbe8b9495010822e73
ouivve <ouivve@gmail.com>
2022-03-24 13:42:02 +0900

    add: 2.7 undefined & null

---
+++ Ch 2. Basic Types/basic-types/null.ts
@@ -0,0 +1,10 @@
+// strictNullChecks => true
+// Type 'null' is not assignable to type 'string'
+// let MyName: string = null;
+
+// let u: undefined = null;
+let v: void = undefined; // void에는 값 또는 undefined만 넣을 수 있음
+
+let union: string | null = null;
+
+union = "ouivve";
