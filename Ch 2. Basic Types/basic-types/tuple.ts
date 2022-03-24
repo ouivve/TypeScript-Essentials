dcd213404985d9b9819130402f504437f13c9a43
ouivve <ouivve@gmail.com>
2022-03-24 14:02:14 +0900

    add: 2.10 Tuple

---
+++ Ch 2. Basic Types/basic-types/tuple.ts
@@ -0,0 +1,12 @@
+let x: [string, number];
+
+x = ["hello", 23];
+
+// x = [10, "hi"]; // Error
+
+// x[3] = "world"; // Error
+
+const person: [string, number] = ["Boram", 26];
+
+// const fisrt: string, const second: number
+const [fisrt, second] = person;
